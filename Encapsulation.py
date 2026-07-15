9b862d5fd995e57467afa43a7dac6471e282d0c2
Nishtha Shukla <shuklanishtha14@gmail.com>
2026-07-15 14:34:26 +0530

    Comment out code examples in Encapsulation.py

--- Encapsulation.py
+++ Encapsulation.py
@@ -1,1 +1,41 @@
+# class Student:
+#     def __init__(self):
+#         self._name = "Yash"
 
+#     def show(self):
+#         print(self._name)
+
+# obj = Student()
+# obj.show()
+# print(obj._name) # can be used but it's not recommended 
+
+
+# Private 
+
+# class Student:
+#     def __init__(self):
+#         self.__name = "Yash"  #Private variable - Double underscore
+
+#     def show(self):
+#         print(self.__name)
+
+# obj = Student()
+# obj.show()
+# print(obj.__name)
+
+
+# Getter 
+
+# class Student:
+#     def __init__(self):
+#         self.__name = "Vineet"
+
+#     def get_mark(self): #getter
+#         return self.__name
+    
+# obj = Student()
+# print(obj.get_mark())
+
+# class Student:
+#     def __init__(self):
+#         self.__marks = 0
